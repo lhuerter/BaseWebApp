4f8e7b49c73b66c9192b84c462848150c6d03674
lhuerter <l.huerter@helmut-meeth.com>
2022-04-29 09:30:51 +0200

    modified:   Procfile 	deleted:    README.md 	deleted:    app.json 	deleted:    firebase/.firebaserc 	deleted:    firebase/firebase.json 	deleted:    firestore/firestore-debug.log     deleted:    firestore/firestore.indexes.json 	deleted:    firestore/firestore.rules 	deleted:    functions/.eslintrc.js 	deleted:    functions/.gitignore 	deleted:    functions/index.js 	deleted:    functions/package.json 	deleted:    project/images/.DS_Store 	modified:   project/js/main.js 	deleted:    public/404.html 	deleted:    public/index.html

--- project/js/main.js
+++ project/js/main.js
@@ -1,21 +1,21 @@
-function addMessage(Tournummer){  
+/*function addMessage(Tournummer){  
   //const postListRef = ref(db, 'posts');
   //const newPostRef = push(postListRef);
   set(ref(db, 'anfragen/' + Tournummer), {
     Tournummer: Tournummer 
   });
-  /*.then(() => {
+  .then(() => {
     // Data saved successfully!
     window.location.reload();
   })
   .catch((error) => {
     // The write failed...
-  });*/
-}
+  });
+}*/
 
 function submitFunction(){
 
   var Tournummer = $("#Tournummer").val();
   console.log(Tournummer);
-  addMessage(Tournummer);
+  //addMessage(Tournummer);
 }
